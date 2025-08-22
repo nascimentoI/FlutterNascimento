8b3f8a5bf434df4bed5629e14e12d59faa234622
nascimentoI <italo.c.nascimento@aluno.senai.br>
2025-08-22 13:51:03 -0300

    Atividade - Value Notifier

---
+++ intro - flutter/flutter_application_1/lib/pages/home_screen_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/custom_card.dart';
+import 'package:flutter_application_1/examples/value_notifier_counter_screen.dart';
+import 'package:flutter_application_1/pages/counter_screen_page.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+      ),
+      body: Padding(padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                CustomCard(
+                  title: 'Counter Screen', 
+                  description: 'Exemplo Contador', 
+                  icon: Icons.add_circle_outline, 
+                  destination: CounterScreenValueNotifier(),
+
+                ),
+              ],
+            ),
+          )
+        ],
+        ),
+      ),      
+    );
+  }
+}
